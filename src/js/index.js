099224283c72c61c43f11b1b02005904366d17fe
Saul Lopez <slopez@LAB-DIC-3.local>
2019-08-06 17:19:35 -0600

    I have added the normalize file

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,13 @@ import '../scss/style.scss';
 // Register service worker.
 import './registerServiceWorker';
 
-// function sendForm(data, url) {
-//   let params = new URLSearchParams();
-//   data.forEach((element, index) => {
-//     params.append(element.name, data[index].value);
-//   });
-//   fetch(`${url}`, {
-//     method: 'POST',
-//     body: params.toString()
-//   })
-// }
-
 function getSongs(data) {
   console.log(data);
 }
 
 function getJson(url, funct) {
   fetch(url)
-    .then((data) => {
-      return data.json()
-    })
+    .then((data) => { return data.json(); })
     .then((data) => {
       if (typeof funct === 'function') {
         funct(data);
@@ -34,6 +21,6 @@ function getJson(url, funct) {
     });
 }
 
-window.onload = function () {
+window.onload = function initial() {
   getJson('./songs.json', getSongs);
 };
